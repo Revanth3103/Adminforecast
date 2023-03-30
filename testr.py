36d04d3bc952e59f931529554b112111fd815b45
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-03-30 12:08:49 +0530

    Update testr.py

--- testr.py
+++ testr.py
@@ -13,6 +13,17 @@ import base64
 
 st.title('Forecasting')
 
+file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
+dataset = st.file_uploader(label = '')
+
+st.sidebar.header('Import Dataset to Use Available Features:')
+
+if dataset:
+    if file_format == 'csv' or use_defo:
+        df = pd.read_csv(dataset)
+    else:
+        df = pd.read_excel(dataset)
+
 st.write('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column be ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y(target) column must be numeric, and represents the measurement we wish to forecast.', type='csv')
 st.write('CSV file can be updated and reuploaded any number of times inorder to get prediction and forecasting depends on only two factors date(ds) and target column(y)')
 
@@ -84,16 +95,6 @@ st.write('<p style="font-size:100%">&nbsp 4. Distribution of target using plot</
 functions.space()
 st.write('<p style="font-size:130%">Import Dataset</p>', unsafe_allow_html=True)
 
-file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
-dataset = st.file_uploader(label = '')
-
-st.sidebar.header('Import Dataset to Use Available Features:')
-
-if dataset:
-    if file_format == 'csv' or use_defo:
-        df = pd.read_csv(dataset)
-    else:
-        df = pd.read_excel(dataset)
     
     st.subheader('Dataframe:')
     n, m = df.shape
