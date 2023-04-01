7a2ef0f59a7ee1af3113dd76fb310726c0a48ad8
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-04-01 18:09:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ st.title('Forecasting')
 
 st.write('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column be ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y(target) column must be numeric, and represents the measurement we wish to forecast.', type='csv')
 st.write('CSV file can be updated and reuploaded any number of times inorder to get prediction and forecasting depends on only two factors date(ds) and target column(y)')
-
+df = st.file_uploader()
 
 if df is not None:
     data = pd.read_csv(df)
