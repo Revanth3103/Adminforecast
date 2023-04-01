828a2c924eb1ca1f76e36a683b5b0153948da44f
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-04-01 18:49:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,10 @@ from prophet.diagnostics import cross_validation
 from prophet.plot import plot_cross_validation_metric
 import base64
 
-st.title('Forecasting')
-st.write('CSV file can be updated and reuploaded any number of times inorder to get prediction and forecasting depends on only two factors date(ds) and target column(y)')
-
-
+st.title(' Price Forecasting')
 df = st.file_uploader('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp.', type='csv')
 
+st.write('CSV file can be updated and reuploaded any number of times inorder to get prediction and forecasting depends on only two factors date(ds) and target column(y)')
 
 if df is not None:
     data = pd.read_csv(df)
