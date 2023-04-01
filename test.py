6d60305c328c4db57cd80209f4fa38f6cc4225fc
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-04-01 18:55:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ st.title(' Price Forecasting')
 df = st.file_uploader('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp.', type='csv')
 
 st.write('CSV file can be updated and reuploaded any number of times inorder to get forecasting, it  depends on only two factors datestamp(ds) and target column(y)')
-st.write('Note: Admin must name ds and y otherwise it will show error'.)
+st.write('Note: Admin must name ds and y otherwise it will show error.')
 
 if df is not None:
     data = pd.read_csv(df)
